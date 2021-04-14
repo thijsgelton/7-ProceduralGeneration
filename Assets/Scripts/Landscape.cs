c6d8552638e375218c7d411f1f44d4ea6f73bc98
Marit19 <47208301+Marit19@users.noreply.github.com>
2021-04-14 09:51:45 +0200

    Update landscape generator + improve GUI

--- Assets/Scripts/Landscape.cs
+++ Assets/Scripts/Landscape.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+// CG&CV: Group 7
+
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
 public class Landscape : MonoBehaviour
 {
     private bool _isDirty;
     private Mesh _mesh;
-    [SerializeField] private TerrainType[] terrainTypes;
+    [SerializeField] private Gradient defaultGradient;
+    [SerializeField] private Gradient buttesGradient;
+    private Gradient gradient;
+
 
     public Slider persistanceSlider, lacunaritySlider, octavesSlider, shiftXSlider, shiftYSlider, scaleSlider;
-    public Toggle toggleFilter;
+    public Toggle buttesFilter;
     
-    [Range(0, 1)] [SerializeField] private float persistance = 0.5f;
+    [Range(0, 1)] [SerializeField] private float gain = 0.5f;
     [Range(1, 3)] [SerializeField] private float lacunarity = 2f;
     [Range(1, 8)] [SerializeField] private int octaves = 4;
  
-    [SerializeField] private float scale = 1f;
+    [SerializeField] private float scale = 5f;
     [SerializeField] private Vector2 shift = Vector2.zero;
     [SerializeField] private int state = 0;
     [SerializeField] private int resolution = 256;
@@ -28,44 +34,54 @@ public class Landscape : MonoBehaviour
     private static float _maxheight = 0f;
 
     private void Awake()
-    {
+    {        
         (GetComponent<MeshFilter>().mesh = _mesh = new Mesh {name = name}).MarkDynamic();
+
+        // set default values
+        gradient = defaultGradient;
+        _isDirty = true;
+
+        // add listeners to sliders
         persistanceSlider.onValueChanged.AddListener(delegate(float newValue)
         {
-            persistance = newValue; GenerateLandscape();  
+            gain = newValue;
+            _isDirty = true;  
         });
         lacunaritySlider.onValueChanged.AddListener(delegate(float newValue)
         {
             lacunarity = newValue;
-            GenerateLandscape();
+            _isDirty = true;
         });
         octavesSlider.onValueChanged.AddListener(delegate(float newValue)
         {
             octaves = (int) newValue;
-            GenerateLandscape();
+            _isDirty = true;
         });
         shiftXSlider.onValueChanged.AddListener(delegate(float newValue)
         {
             shift.x = newValue;
-            GenerateLandscape();
+            _isDirty = true;
         });
         shiftYSlider.onValueChanged.AddListener(delegate(float newValue)
         {
             shift.y = newValue;
-            GenerateLandscape();
+            _isDirty = true;
         });
         scaleSlider.onValueChanged.AddListener(delegate(float newValue)
         {
             scale = newValue;
-            GenerateLandscape();
+            _isDirty = true;
         });
-        toggleFilter.onValueChanged.AddListener(delegate(bool isOn)
+        buttesFilter.onValueChanged.AddListener(delegate(bool isOn)
         {
-            GenerateLandscape();
+            if(isOn) {
+                gradient = buttesGradient;
+            } else {
+                gradient = defaultGradient;
+            }
+            _isDirty = true;
         });
-        
     }
-
 
     private void OnValidate()
     {
@@ -82,60 +98,73 @@ public class Landscape : MonoBehaviour
     private void GenerateLandscape()
     {
         // First, initialize the data structures:
-        var numberOfVertices = (resolution + 1) * (resolution + 1);
-        var colors = new Color[numberOfVertices];
-        var triangles = new int[resolution * resolution * 6];
-        var vertices = new Vector3[numberOfVertices];
+        int n_vertices = resolution * resolution;
+        int n_triangles = (resolution-1) * (resolution-1) * 2; //(int) length; // ????
+        var colors = new Color[n_vertices];
+        var triangles = new int[n_triangles * 3];
+        var vertices = new Vector3[n_vertices];
         _maxheight = 0f;
 
         // Then, loop over the vertices and populate the data structures:
-        for(int i = 0, z = 0; z <= resolution; z++)
+        var i = 0;      // vertices counter
+        for (int z = 0; z < resolution; z++)
         {
-            for (var x = 0; x <= resolution; x++)
+            for(var x = 0; x < resolution; x++)
             {
-                var coords = new Vector2((float) x / resolution,  (float) z / resolution);
-                var elevation =  1.414214f * FractalNoise(coords, persistance, lacunarity, octaves, scale, shift, state);
-                foreach (var terrainType in terrainTypes)
+                // get coordinates
+                var coords = new Vector2((float) x / (resolution - 1), (float) z / (resolution - 1));
+                
+                // compute elevation
+                var elevation = FractalNoise(coords, gain, lacunarity, octaves, scale, shift, state);
+                //var elevation = 1.414214f * FractalNoise(coords, gain, lacunarity, octaves, scale, shift, state);
+
+                // set color and create vertices
+                colors[i]   = gradient.Evaluate(elevation);
+                vertices[i] = new Vector3(length * coords.x, height * elevation, length * coords.y);
+                
+                // save max height for Buttes filter
+                if (height * elevation > _maxheight)
                 {
-                    if (!(elevation <= terrainType.height)) continue;
-                    colors[i] = terrainType.color;
-                    break;
+                    _maxheight = height * elevation;
                 }
 
-                var fractalHeight = height * elevation;
-                vertices[i] = new Vector3(length * coords.x,  fractalHeight, length * coords.y);
-                if (fractalHeight > _maxheight)
-                {
-                    _maxheight = fractalHeight;
-                }
+                // increment counter
                 i++;
             }
         }
         
         // Then looping over the vertices and producing the triangles 
-        var tris = 0;
-        var vert = 0;
-        for (var z = 0; z < resolution; z++) {
-            for(var x = 0; x < resolution; x++) {
-                triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + resolution + 1;
-                triangles[tris + 2] = vert + 1;
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + resolution + 1;   
-                triangles[tris + 5] = vert + resolution + 2;
+        i = 0;          // vertices counter
+        var tr = 0;     // triangle counter
+        for (int z = 0; z < resolution-1; z++)
+        {
+            for(var x = 0; x < resolution-1; x++)
+            {
+                triangles[tr + 0] = i + 0;
+                triangles[tr + 1] = i + (resolution-1) + 1;
+                triangles[tr + 2] = i + 1;
+                triangles[tr + 3] = i + 1;
+                triangles[tr + 4] = i + (resolution-1) + 1;
+                triangles[tr + 5] = i + (resolution-1) + 2;
 
-                if (toggleFilter.isOn) {
-                    var vertex = vertices[vert];
-                    vertices[vert].y = ButtesFilter(vertex.y);
+                // apply Buttes filter
+                if (buttesFilter.isOn) {
+                    var vertex = vertices[i];
+                    vertices[i].y = ButtesFilter(vertex.y);
                 }
                 
-                vert++;
-                tris += 6;
-
+                // increment counters
+                i++;
+                tr += 6;
             }
-
-            vert++;
-
+            // apply Buttes filter
+            if (buttesFilter.isOn) {
+                var vertex = vertices[i];
+                vertices[i].y = ButtesFilter(vertex.y);
+            }
+            
+            // increment counter
+            i++;
         }
 
         // Assign the data structures to the mesh
@@ -146,43 +175,47 @@ public class Landscape : MonoBehaviour
         _mesh.RecalculateNormals();
     }
  
-    private static float FractalNoise(Vector2 coords, float persistance, float lacunarity, int octaves, float scale,
+    private static float FractalNoise(Vector2 coords, float gain, float lacunarity, int octaves, float scale,
         Vector2 shift, int state)
     {
-        var fractalNoise = 0f;
+        var noise = 0.0f;
+        var frequency = 1.0f;
+        var amplitude = 1.0f;
         Random.InitState(state);
-        for (var i = 0; i < octaves; i++)
+
+        // Loop over octaves to compute fractal noise
+        for(int i = 0; i < octaves; i++)
         {
-            var frequency = Mathf.Pow(lacunarity, i);
-            var amplitude = Mathf.Pow(persistance, i);
-            var x = coords.x / scale * frequency + Random.value + shift.x;
-            var y = coords.y / scale * frequency + Random.value + shift.y;
-            fractalNoise += Mathf.PerlinNoise(x, y) * amplitude;
+            // get x and y values
+            var x = coords.x * frequency * scale + Random.value + shift.x;
+            var y = coords.y * frequency * scale + Random.value + shift.y;
+
+            // compute new noise with Perlin noise
+            noise = noise + (Mathf.PerlinNoise(x, y) * amplitude);
+
+            // update frequency and amplitude
+            frequency = frequency * lacunarity;
+            amplitude = amplitude * gain;
         }
 
-        return fractalNoise;
+        return noise;
     }
 
+    // Function to make Buttes'
+    // Calculates the new height for one location
     private float ButtesFilter(float _height)
     {
+        // Use the logistic function to get an S-curve
         var halfMax = _maxheight / 2;
         var scaledHeight = sharpness * ((_height - halfMax) / halfMax);
-        var logistic = 1 / (1 + Mathf.Exp(scaledHeight));
+        // Take sigmoid
+        var logistic = 1 / (1 + Mathf.Exp(-scaledHeight));
         return _maxheight * (1 + logistic / 2);
     }
     
-    private float WaterFilter(float _height)
-    {
-        var waterHeight = terrainTypes[0].height * height;
-        return _height <= waterHeight ? waterHeight: _height;
-    } 
-
-}
-
-[Serializable]
-public struct TerrainType
-{
-    public string name;
-    public float height;
-    public Color color;
+    // private float WaterFilter(float _height)
+    // {
+    //     var waterHeight = terrainTypes[0].height * height;
+    //     return _height <= waterHeight ? waterHeight: _height;
+    // }
 }
